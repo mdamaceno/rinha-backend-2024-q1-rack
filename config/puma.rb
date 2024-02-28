edb123801b4a7f881fcc0458c2d5e2d9f17b958f
Marco Damaceno <maadamaceno@gmail.com>
2024-02-28 18:40:18 -0300

    remove puma

--- config/puma.rb
+++
@@ -1,8 +0,0 @@
-max_threads_count = ENV.fetch("APP_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("APP_MIN_THREADS") { max_threads_count }
-
-threads min_threads_count, max_threads_count
-
-port 3000
-environment "production"
-worker_timeout 30000
